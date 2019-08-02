9e2aab77772775fa8762f137ee653dc2d6e3fbe5
sfujiwara <shuhei.fujiwara@gmail.com>
2019-08-03 02:52:21 +0900

    fix modules

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+echo "Load modules..."
 module purge
 module load python/3.6.5
 module load cuda/10.0.130
-module load intel cuda openmpi
+module load cudnn/7.4
+module load nccl/2.4.2
+module load openmpi/2.1.2-opa10.9-t3
 
 # Show loaded modules
+echo "Show module list"
 module list
 
 # Create virtual environment
+echo "Create virtual environment..."
 python3 -m venv ~/tf
 source ~/tf/bin/activate
 
 # Install Python packages
+echo "Install Python packages..."
 python3.6 -m pip install -U pip
 pip install tensorflow-gpu==1.14.0
 pip install horovod==0.13.11
