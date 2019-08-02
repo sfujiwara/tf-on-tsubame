9b4650d79ee8c56eff7989c65715d692c18bc673
sfujiwara <shuhei.fujiwara@gmail.com>
2019-08-03 02:28:43 +0900

    fix load_modules.sh

--- bin/load_modules.sh
+++ bin/load_modules.sh
@@ -5,8 +5,5 @@ module load python/3.6.5
 module load cuda/10.0.130
 module load intel cuda openmpi
 
-# Create virtual environment
-python3 -m venv tf
-source tf/bin/activate
-
-# pip install --user python modules
+# Show loaded modules
+module list
