6ce6dfd46c21cb508c450f0c13eac3f2691adbbf
sfujiwara <shuhei.fujiwara@gmail.com>
2019-08-05 15:38:26 +0900

    change versions

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,8 @@
 echo "Load modules..."
 module purge
 module load python/3.6.5
-module load cuda/10.0.130
+# module load cuda/10.0.130
+module load cuda/9.2.148
 module load cudnn/7.4
 module load nccl/2.4.2
 module load openmpi/2.1.2-opa10.9-t3
@@ -18,7 +19,8 @@ source ~/tf/bin/activate
 # Install Python packages
 echo "Install Python packages..."
 python3.6 -m pip install -U pip
-pip install tensorflow-gpu==1.14.0
+# pip install tensorflow-gpu==1.14.0
+pip install tensorflow-gpu==1.12.0
 pip install horovod==0.13.11
 
 # Show Python packages
