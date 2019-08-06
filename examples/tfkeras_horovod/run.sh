4d643aeeaff6aba92b77d9f6d62339cbd7f9f69b
sfujiwara <shuhei.fujiwara@gmail.com>
2019-08-06 14:38:52 +0900

    horovod with mpirun

--- examples/tfkeras_horovod/run.sh
+++ examples/tfkeras_horovod/run.sh
@@ -12,7 +12,7 @@
 # Load modules
 module purge
 module load python/3.6.5
-module load cuda/9.2.148
+module load cuda/9.0.176
 module load cudnn/7.4
 module load nccl/2.4.2
 module load openmpi/2.1.2-opa10.9-t3
@@ -21,4 +21,4 @@ module load openmpi/2.1.2-opa10.9-t3
 source ~/venv-tf1.12/bin/activate
 
 # Run script
-python task.py
+mpirun -npernode 4 -n 8 -x LD_LIBRARY_PATH python task.py
