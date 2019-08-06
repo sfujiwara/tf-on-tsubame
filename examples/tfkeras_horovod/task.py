19c7bda4942576aad4eedb5f3653edf6e3eab248
sfujiwara <shuhei.fujiwara@gmail.com>
2019-08-06 15:39:58 +0900

    pin gpu

--- examples/tfkeras_horovod/task.py
+++ examples/tfkeras_horovod/task.py
@@ -46,6 +46,13 @@ def main():
     # Initialize horovod
     hvd.init()
 
+    # Horovod: pin GPU to be used to process local rank (one GPU per process)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    for gpu in gpus:
+        tf.config.experimental.set_memory_growth(gpu, True)
+    if gpus:
+        tf.config.experimental.set_visible_devices(gpus[hvd.local_rank()], 'GPU')
+
     model = create_model()
 
     # Wrap keras optimizer with horovod distributed optimizer
